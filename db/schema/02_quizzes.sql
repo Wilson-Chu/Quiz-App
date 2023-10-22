b5022f54a3d5a19a78a40f3744b86d1c7e7fea7c
Alfonso <alfonsobanzon@gmail.com>
2023-10-22 14:20:27 -0400

    added date_created to schema 02_quizzes.sql

--- db/schema/02_quizzes.sql
+++ db/schema/02_quizzes.sql
@@ -4,5 +4,6 @@ CREATE TABLE quizzes (
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE NOT NULL,
   title VARCHAR(255) NOT NULL,
   description VARCHAR(255) DEFAULT '',
+  date_created DATE DEFAULT NOW(),
   private BOOLEAN DEFAULT FALSE
 );
