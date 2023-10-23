1250a09ce8b2810427208d8ac2b771fccff45317
Alfonso <alfonsobanzon@gmail.com>
2023-10-23 17:46:28 -0400

    add questions button functional

--- public/scripts/new-quiz.js
+++ public/scripts/new-quiz.js
@@ -1,3 +1,24 @@
-$(()=>{
+$(() => {
+
+  $('.add-question').on('click', (evt) => {
+
+    const question = `
+    <div class="new-question">
+    <textarea name="question" placeholder="Question"></textarea>
+    <textarea name="answer" placeholder="Correct Answer"></textarea>
+    <textarea name="answer" placeholder="Option 1"></textarea>
+    <textarea name="answer" placeholder="Option 2"></textarea>
+    <textarea name="answer" placeholder="Option 3"></textarea>
+    <button class="delete-question" type="button">Delete Question</button>
+    </div>
+    `;
+
+    $('.question-container').append(question);
+
+  });
+  
+  $(document).on('click', '.delete-question', (evt) => {
+    $('.delete-question').parent().remove();
+  });
 
 });
