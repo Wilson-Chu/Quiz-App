d85e1e83d461d25b62ba12c0101bc6ce949271dc
Bevan Saldanha <bevansaldanha15@gmail.com>
2023-10-24 13:08:48 -0400

    login fix

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../db/queries/login');
 
 router.get('/', (req, res) => {
   res.render('login');
@@ -23,8 +24,7 @@ router.post('/', (req, res) => {
       return res.send({ error: "error" });
     }
 
-
-    // req.session.userId = user.id;
+    req.session.userId = user.id;
     res.redirect('/quizzes');
   });
 });
