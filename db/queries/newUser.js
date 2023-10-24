d667233b0fee5093dbeba900a82f73d5b78d4016
Wilson Chu <wilson1.chu@gmail.com>
2023-10-24 13:37:06 -0400

    update register to create new user in DB, need to work on userID retrieval

---
+++ db/queries/newUser.js
@@ -0,0 +1,22 @@
+const e = require('express');
+const db = require('../connection');
+
+const createNewUser = (input) => {
+
+  const { username, email, password } = input;
+
+  return db.query(`
+  INSERT INTO users (username, email, password)
+  VALUES($1, $2, $3)
+  RETURNING *`,
+  [username, email, password])
+    .then(data => {
+      console.log(data.rows);
+      return data.rows;
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+};
+
+module.exports = { createNewUser };
--- routes/register.js
+++ routes/register.js
@@ -1,8 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const { authenticateUser, getUserByEmail, generateRandomString } = require('../helpers');
+const dbUsers = require('../db/queries/users');
+const dbNewUser = require('../db/queries/newUser');
+const bcrypt = require("bcryptjs");
+
+const users = dbUsers;
 
 router.get('/', (req, res) => {
-  res.render('register');
+  const templateVars = { user: authenticateUser(req.session.user_id, users) };
+
+  // if (req.session.user_id) {
+  //   return res.redirect("/quizzes");
+  // }
+
+  res.render("register", templateVars);
+});
+
+router.post('/', (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return res.status(400).send('Invalid information - please provide email or password');
+  }
+
+  if (getUserByEmail(email, users)) {
+    return res.status(400).send("Email already taken. Please try another one.");
+  }
+
+  // ADD NEW USER AND INFO TO PSQL DATABASE
+  dbNewUser.createNewUser({ username: username, email: email, password: bcrypt.hashSync(password, 10) });
+
+  req.session.user_id = randomUserId; // how do I retrieve the user_id of new user ???
+
+  res.redirect("/quizzes");
 });
 
 module.exports = router;
