dfe99bbb21f40f07929c35a2f35d120051277c88
Wilson Chu <wilson1.chu@gmail.com>
2023-10-24 03:33:55 -0400

    update /logout to handle redirects to /login

--- routes/logout.js
+++ routes/logout.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.redirect('login');
+});
+
 router.post('/', (req, res) => {
   // req.session = null;
   res.redirect('login');
