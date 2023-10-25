220dfc346b71a2dc8d91c07fa14b41ccb2cf2c65
Bevan Saldanha <bevansaldanha15@gmail.com>
2023-10-25 12:21:17 -0400

    add url_id to table

--- db/seeds/02_quizzes.sql
+++ db/seeds/02_quizzes.sql
@@ -1,5 +1,5 @@
-INSERT INTO quizzes (owner_id, title, private, description)
+INSERT INTO quizzes (owner_id, title, private, description,url_id)
 VALUES 
-(1, 'CSS', false, 'description'),
-(2, 'SQL', false, 'description'),
-(1, 'JavaScript', true, 'description');
+(1, 'CSS', false, 'description','lnmlx36'),
+(2, 'SQL', false, 'description','f8hd8w0'),
+(1, 'JavaScript', true, 'description','i5cvxwp');
