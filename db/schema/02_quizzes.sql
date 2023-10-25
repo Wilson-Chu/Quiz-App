77e7ffa87a5a94ce6a3b67b92c1bcfdbd60fd841
Alfonso <alfonsobanzon@gmail.com>
2023-10-25 15:04:40 -0400

    added css to create quiz

--- db/schema/02_quizzes.sql
+++ db/schema/02_quizzes.sql
@@ -3,7 +3,7 @@ CREATE TABLE quizzes (
   id SERIAL PRIMARY KEY NOT NULL,
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE NOT NULL,
   title VARCHAR(255) NOT NULL,
-  description VARCHAR(255) DEFAULT '',
+  description TEXT DEFAULT '',
   date_created DATE DEFAULT NOW(),
   private BOOLEAN DEFAULT FALSE
 );
--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,4 +1,4 @@
 -- Users table seeds here (Example)
-INSERT INTO users (username, email, password, bio) VALUES ('Alice', 'alice@wonderland.com', 'password', 'Hello World!');
-INSERT INTO users (username, email, password, bio) VALUES ('Kira', 'kira@example.com', 'password', 'Hello World!');
-INSERT INTO users (username, email, password, bio) VALUES ('Cool Username', 'coolname@gmail.com', 'password', '10/10 Perfect Score!');
+INSERT INTO users (username, email, password, bio) VALUES ('Alice', 'alice@wonderland.com', '$2a$10$kBV00w.nvGWZk47oOv90ieTw2KXV.O4KiGPnS.di8LrRu223FkIr.', 'Hello World!');
+INSERT INTO users (username, email, password, bio) VALUES ('Kira', 'kira@example.com', '$2a$10$kBV00w.nvGWZk47oOv90ieTw2KXV.O4KiGPnS.di8LrRu223FkIr.', 'Hello World!');
+INSERT INTO users (username, email, password, bio) VALUES ('Cool Username', 'coolname@gmail.com', '$2a$10$kBV00w.nvGWZk47oOv90ieTw2KXV.O4KiGPnS.di8LrRu223FkIr.', '10/10 Perfect Score!');
