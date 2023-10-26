e9cde6832a2a04ac74c39f3554ce42bec656c8da
Alfonso <alfonsobanzon@gmail.com>
2023-10-26 13:58:25 -0400

    added answer randomization

--- public/scripts/quiz-show.js
+++ public/scripts/quiz-show.js
@@ -4,50 +4,88 @@ $(document).ready(function() {
   let id = url.split('/').pop();
   console.log(id);
 
+  const shuffle = function(array) {
+    let currentIndex = array.length;
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+  
+      // Pick a remaining element.
+      const randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+    return array;
+  }
+
   const createQuestionElement = (data) => {
     console.log(data);
 
-
+    const a = `<label> <input type="radio" name="answer" id="a" value="${data.answer}"/> ${data.answer} </label>`;
+    const q1 = `<label> <input type="radio" name="answer" id="b" value="${data.option_1}"/> ${data.option_1} </label>`;
+  
+    qArr = [a, q1];
+  
+    if (data.option_2){
+      const q2 = `<label> <input type="radio" name="answer" id="a" value="${data.option_2}"/> ${data.option_2} </label>`;
+      qArr.push(q2);
+    }
+  
+    if (data.option_2){
+      const q3 = `<label> <input type="radio" name="answer" id="a" value="${data.option_3}"/> ${data.option_3} </label>`;
+      qArr.push(q3);
+    }
+  
+    console.log('QARR', qArr);
+    const shuffledArr = shuffle(qArr);
 
     let layout = `
-    
-    
     <h2>${data.question}</h2>
-
     <div id="answers">
-    
-    <label> <input type="radio" name="answer" id="a" value="${data.answer}"/> ${data.answer} </label>
-    <label> <input type="radio" name="answer" id="b" value="${data.option_1}"/> ${data.option_1} </label>
-    `;
+    `
 
-    //check if option 2 & 3 have beeen created
-    if (data.option_2) {
-      layout += `    <label> <input type="radio" name="answer" id="c" value="${data.option_2}"/> ${data.option_2} </label>
-      `;
-    }
-    if (data.option_3) {
-      layout += `    <label> <input type="radio" name="answer" id="d" value="${data.option_3}"/> ${data.option_3} </label>
-      `;
-    }
+    shuffledArr.forEach (each => {
+      layout += each
+    })
+
+    // let layout = `
+    // <h2>${data.question}</h2>
+    // <div id="answers">
+    // <label> <input type="radio" name="answer" id="a" value="${data.answer}"/> ${data.answer} </label>
+    // <label> <input type="radio" name="answer" id="b" value="${data.option_1}"/> ${data.option_1} </label>
+    // `;
+
+    // //check if option 2 & 3 have beeen created
+    // if (data.option_2) {
+    //   layout += `    <label> <input type="radio" name="answer" id="c" value="${data.option_2}"/> ${data.option_2} </label>
+    //   `;
+    // }
+
+    // if (data.option_3) {
+    //   layout += `    <label> <input type="radio" name="answer" id="d" value="${data.option_3}"/> ${data.option_3} </label>
+    //   `;
+    // }
 
     layout += `</div>`;
     return layout;
   };
 
-  const renderQuiz = ((quiz) => {
+  // const renderQuiz = ((quiz) => {
 
-    if (!quiz[0]) {
-      $('main').append(`<h2>The creator hasn't added questions yet!🥲</h2>`);
-      return;
-    }
+  //   if (!quiz[0]) {
+  //     $('main').append(`<h2>The creator hasn't added questions yet!🥲</h2>`);
+  //     return;
+  //   }
 
-    quiz.forEach((q) => {
+  //   quiz.forEach((q) => {
 
-      const quizElement = createQuestionElement(q);
+  //     const quizElement = createQuestionElement(q);
 
-      $('#quiz-container').append(quizElement);
-    });
-  });
+  //     $('#quiz-container').append(quizElement);
+  //   });
+  // });
 
   $.get(`/api/quiz-by-id/${id}`, function(data) {
     console.log(data)
